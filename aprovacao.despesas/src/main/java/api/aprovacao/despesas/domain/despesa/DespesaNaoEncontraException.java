1bda1dfe97179540cb41900c7e909412b487bff4
wanderson <wandersonluiz200@gmail.com>
2026-08-21 10:14:32 -0300

    refector: corrige nome da exception DespesaNaoEncontradaException

--- aprovacao.despesas/src/main/java/api/aprovacao/despesas/domain/despesa/DespesaNaoEncontraException.java
+++
@@ -1,7 +0,0 @@
-package api.aprovacao.despesas.domain.despesa;
-
-public class DespesaNaoEncontraException extends RuntimeException {
-    public DespesaNaoEncontraException() {
-        super("Despesa não encontrada");
-    }
-}
