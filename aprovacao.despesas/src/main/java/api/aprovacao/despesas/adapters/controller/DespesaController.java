da3f7f7a9e75ce000af44a1e664b3c4fa1cfbf2c
wanderson <wandersonluiz200@gmail.com>
2026-08-24 12:19:03 -0300

    refector: organiza as classes DespesaController e FuncionarioController na pasta controller

--- aprovacao.despesas/src/main/java/api/aprovacao/despesas/adapters/controller/DespesaController.java
+++ aprovacao.despesas/src/main/java/api/aprovacao/despesas/adapters/controller/DespesaController.java
@@ -1,4 +1,4 @@
-package api.aprovacao.despesas.adapters.inbound;
+package api.aprovacao.despesas.adapters.controller;
 
 import api.aprovacao.despesas.application.AprovarDespesaUseCase;
 import api.aprovacao.despesas.application.CriarDespesaUseCase;
