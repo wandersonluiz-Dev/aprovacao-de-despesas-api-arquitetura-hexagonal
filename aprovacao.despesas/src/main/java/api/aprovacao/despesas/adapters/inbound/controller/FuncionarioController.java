118bdcdc9457c649dfede224ed2b5560d8a71602
wanderson <wandersonluiz200@gmail.com>
2026-08-31 10:49:40 -0300

    docs: adiciona anotações @Operation nos endpoints

--- aprovacao.despesas/src/main/java/api/aprovacao/despesas/adapters/inbound/controller/FuncionarioController.java
+++ aprovacao.despesas/src/main/java/api/aprovacao/despesas/adapters/inbound/controller/FuncionarioController.java
@@ -2,6 +2,8 @@ package api.aprovacao.despesas.adapters.inbound.controller;
 
 import api.aprovacao.despesas.application.CriarFuncionarioUseCase;
 import api.aprovacao.despesas.domain.funcionario.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/funcionario")
 @RequiredArgsConstructor
+@Tag(name = "Funcionário", description = "Gerenciador de funcionários")
 public class FuncionarioController {
 
     private final FuncionarioRepositoryPort funcionarioRepositoryPort;
@@ -19,6 +22,7 @@ public class FuncionarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "cria um novo funcionário que não tenha cadastro")
     public FuncionarioResponseDto criar(@RequestBody FuncionarioRequestDto request) {
         Funcionario funcionario = funcionarioUseCase.criarFuncionario(
                 request.nomeFuncionario(),
@@ -30,6 +34,7 @@ public class FuncionarioController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "busca funcionários por ID")
     public FuncionarioResponseDto buscarPorId(@PathVariable Long id) {
         Funcionario funcionario = funcionarioRepositoryPort.buscarPorId(id)
                 .orElseThrow(FuncionarioNaoEncontradoException::new);
@@ -39,6 +44,7 @@ public class FuncionarioController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation( summary = "busca todos os funcionários que tem persistência no banco de dados")
     public List<FuncionarioResponseDto> listarTodos() {
         List<Funcionario> funcionarios = funcionarioRepositoryPort.listarTodos();
 
