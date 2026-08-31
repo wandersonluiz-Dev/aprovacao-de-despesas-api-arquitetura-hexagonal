6e263a8c0462ac449264c7529648962ecadb3489
wanderson <wandersonluiz200@gmail.com>
2026-08-31 10:57:57 -0300

    docs: configura documentação da API com Swagger

---
+++ aprovacao.despesas/src/main/java/api/aprovacao/despesas/infrastructure/SwaggerConfiguration.java
@@ -0,0 +1,19 @@
+package api.aprovacao.despesas.infrastructure;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI customAPI() {
+
+        return new OpenAPI().info(new Info()
+                .title("Aprovação de despesas")
+                .description("Documentação do aprovação de despesa API")
+        );
+    }
+}
